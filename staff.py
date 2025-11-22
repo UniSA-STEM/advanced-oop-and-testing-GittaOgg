f2b37cd54c44aab43cffb502675f579f78083438
GittaOgg <tashhunterisme@gmail.com>
2025-11-22 17:51:31 +0800

    * Schedules set up for Zookeeper, Vet and Gift Shop workers * add_enclosure, add_animal, clean_enclosure, feed_animal methods added to Zookeeper * add_animal, perform_health_check methods added to Vet *

--- staff.py
+++ staff.py
@@ -8,14 +8,18 @@ This is my own work as defined by the University's Academic Integrity Policy.
 """
 
 from abc import ABC, abstractmethod
+import random
 
 class Staff(ABC):
+
+    Staff_list = []
 
     def __init__(self, name, dob, start_date, pay):
         self.name = name
         self.dob = dob
         self.start_date = start_date
         self.pay = pay
+        Staff.Staff_list.append(self)
 
 
     def __str__(self):
@@ -38,45 +42,123 @@ class Zookeeper(Staff):
         super().__init__(name,dob,start_date,pay)
         self.enclosures_to_clean = []
         self.animals_to_feed = []
+        self.max_enclosures = 5
+        self.max_animals = 10
 
     def __str__(self):
         return '**** ZOOKEEPER ****' + super().__str__()
 
     def create_schedule(self):
-        pass
+        time_list = ['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00pm','1:00pm','2:00pm']
 
-    def check_schedule(self):
-        pass
+        cleaning_list = []
+        for enclosure in self.enclosures_to_clean:
+            cleaning_list.append(f'Clean {enclosure.name} enclosure')
+
+        feeding_list = []
+        for animal in self.animals_to_feed:
+            feeding_list.append(f'Feed {animal.dietary_requirements} to {animal.name} the {animal.species}')
+
+        work_list = cleaning_list + feeding_list
+
+        while len(work_list) < 9:
+            work_list.append('Ad hoc duties')
+
+        self.schedule = dict(zip(time_list, work_list))
+
+        for key, value in self.schedule.items():
+            print(f'{key}: {value}')
+
+
 
     def add_enclosure(self, enclosure):
-        pass
+        if len(self.enclosures_to_clean) < self.max_enclosures:
+            self.enclosures_to_clean.append(enclosure)
+        else:
+            print(f'{self.name} is assigned {self.max_enclosures} enclosures and cannot be assigned any more. Please '
+                  f'find another staff member to clean this enclosure')
 
     def add_animal(self, animal):
-        pass
+        if len(self.animals_to_feed) < self.max_animals:
+            self.animals_to_feed.append(animal)
+        else:
+            print(f'{self.name} is assigned {self.max_animals} animals and cannot be assigned any more. Please find '
+                  f'another staff member to feed this animal')
 
-    def clean_enclosure(self, enclosure):
-        pass
+    def clean_enclosures(self):
+        for enclosure in self.enclosures_to_clean:
+            enclosure.set_cleanliness(5)
+            print(f'{enclosure.name} has been cleaned to {enclosure.cleanlines} star rating')
+
 
     def feed_animals(self):
-        pass
+        for animal in self.animals_to_feed:
+            print(f'{animal.name} has been fed {animal.dietary_requirements}')
 
 
 class Vet(Staff):
     def __init__(self, name, dob, start_date, pay):
         super().__init__(name,dob,start_date,pay)
         self.animals_under_care = []
+        self.max_animals = 50
 
     def __str__(self):
         return '**** VET ****' + super().__str__()
 
     def create_schedule(self):
-        pass
+        time_list = ['9:00am','10:00am','11:00am','12:00pm','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm']
+        surgery_list = []
+        therapy_list = []
+        first_aid_list = []
+        health_check_list = []
+        for animal in self.animals_under_care:
+            health_check_list.append(f'{animal.name} health check')
+            for issue in animal.health_issues:
+                if issue.treatment_plan=='surgery':
+                    surgery_list.append(f'{animal.name} surgery for {issue}')
+                elif issue.treatment_plan=='therapy':
+                    therapy_list.append(f'{animal.name} therapy for {issue}')
+                elif issue.treatment_plan=='first_aid':
+                    first_aid_list.append(f'{animal.name} first aid for {issue}')
+        work_list = surgery_list + therapy_list + first_aid_list + health_check_list
 
-    def check_schedule(self):
-        pass
+        while len(work_list) < 9:
+            work_list.append('Ad hoc duties')
 
-    def perform_health_check(self):
-        pass
+        while len(work_list) > 9:
+            overflow = []
+            overflow.append(work_list[9])
+            work_list.pop(9)
+            overflow_str = ''
+            for item in overflow:
+                overflow_str += f'{item}\n'
+
+        self.schedule = dict(zip(time_list, work_list))
+
+        for key, value in self.schedule.items():
+            print(f'{key}: {value}')
+
+        print(f'Extra Work List: \n{overflow_str}')
+
+
+    def add_animal(self, animal):
+        if len(self.animals_under_care) < self.max_animals:
+            self.animals_under_care.append(animal)
+        else:
+            print(f'{self.name} is assigned {self.max_animals} animals and cannot be assigned any more. Please find '
+                  f'another staff member to provide care for this animal')
+
+
+    def perform_health_check(self, animal):
+        sick = random.randint(1,10)
+        if animal.health_status =='perfect' and sick == 10:
+            animal.health_status = 'under treatment'
+            print(f'{animal.name} has been declared under treatment')
+        elif animal.health_status == 'under treatment' and sick > 5:
+            animal.health_status = 'perfect'
+            print(f'{animal.name} has been declared healed')
+        else:
+            print(f'{animal.name} is still {animal.health_status}')
 
     def perform_surgery(self):
         pass
@@ -97,7 +179,7 @@ class GiftShop(Staff):
         return '**** GIFT SHOP WORKER ****' + super().__str__()
 
     def create_schedule(self):
-        schedule = {
+        self.schedule = {
             '9:00am': 'Open Shop',
             '9:30am': 'Serve Customers',
             '10:00am': 'Serve Customers',
@@ -114,12 +196,11 @@ class GiftShop(Staff):
             '3:30pm': 'Serve Customers',
             '4:00pm': 'Close Shop',
         }
-        self.schedule = schedule
 
-
-    def check_schedule(self):
         for key, value in self.schedule.items():
             print(f'{key}: {value}')
+
+
 
     def open_shop(self):
         return f'{self.name} has opened the shop.'
