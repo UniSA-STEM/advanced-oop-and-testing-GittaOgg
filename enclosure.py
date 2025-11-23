d67fbde8c3e0a7e340098452327a9351b9232b8d
GittaOgg <tashhunterisme@gmail.com>
2025-11-23 16:09:32 +0800

    * Added doctstrings, comments, type hints * Updated add_animal to include checking for species compatibility, environmental compatibility

--- enclosure.py
+++ enclosure.py
@@ -1,43 +1,113 @@
 """
 File: enclosure.py
-Description: ****DESCRIPTION*****
+Description: Enclosure module for advanced programming assignment containing enclosure class
 Author: Natasha Hunter
 ID: 110439590
 Username: hunny006
 This is my own work as defined by the University's Academic Integrity Policy.
 """
-
+import animal
 
 class Enclosure():
+    '''
+    A class which represents an enclosure
+
+    Attributes
+    ----------
+    name: str
+        Name of the enclosure
+    size: str
+        Size of the enclosure
+    environment: str
+        Environment of the enclosure
+    cleanliness: int
+        Cleanliness rating (1-5) of the enclosure
+    animals: list
+        List of animals within the enclosure
+
+
+    Methods
+    -------
+    add_animal()
+        returns message explaining if successful or unsuccessful in adding animal to enclosure
+    remove_animal()
+        returns message explaining if successful or unsuccessful in removing animal from enclosure
+
+    '''
+
+    Enclosure_list = []
+    # set up of list of enclosures instantiated, for reporting purposes
+
 
     environments = {'Savannah','Aviary','Jungle','Aquatic','Desert','Arctic'}
+    # set of allowable environments for any enclosure to have
+
     sizes = {'Small','Medium','Large','Extra Large'}
+    # set of allowable sizes for any enclosure to be
+
+    size_limits = {
+        'Small': 5,
+        'Medium': 10,
+        'Large': 15,
+        'Extra Large': 20
+    }
+    # dictionary of sizes and max allowable animals
+
+    environment_animals = {
+        'Lion': 'Savannah',
+        'Giraffe': 'Savannah',
+        'Cockatoo': 'Aviary',
+        'Flamingo': 'Aviary',
+        'Tiger': 'Jungle',
+        'Monkey': 'Jungle',
+        'Snake':'Jungle',
+        'Lizard':'Desert',
+        'Camel':'Desert',
+        'Polar Bear':'Arctic',
+        'Penguin':'Arctic'
+    }
+    # dictionary of animals and their environments
 
     def set_environment(self, environment):
+        '''
+        A method to set an enclosure's environment if the environment exists in the set 'environments'
+        :param environment: str
+        :return: ValueError or setting of environment
+        '''
         if environment not in Enclosure.environments:
             raise ValueError('Invalid environment')
         else:
             self.__environment = environment
 
     def set_size(self, size):
+        '''
+        A method to set an enclosure's size if the size exists in the set 'sizes'
+        :param size: str
+        :return: ValueError or setting of size
+        '''
         if size not in Enclosure.sizes:
             raise ValueError('Invalid size')
         else:
             self.__size = size
 
-    def __init__(self, name, size, environment):
+    def __init__(self, name:str, size:str, environment:str):
         try:
             self.__name = name
             self.set_size(size)
             self.set_environment(environment)
             self.__cleanliness = 5
             self.__animals = []
+            Enclosure.Enclosure_list.append(self)
         except ValueError as e:
             print(e)
 
     def __str__(self):
-       return(f'{self.name} is a {self.size} enclosure of type: {self.environment}. Current cleanliness: '
-             f'{self.cleanliness} Stars. ADD ANIMALS!!')
+       Animal_str = ''
+       for animal in self.__animals:
+           Animal_str += '* ' + animal + '\n'
+
+       return(f'**** {self.name} ****\nSize: {self.size} \nType: {self.environment}\nCurrent cleanliness: '
+             f'{self.cleanliness} Stars\nAnimals:\n{Animal_str}\n')
 
     def get_cleanliness(self):
         return self.__cleanliness
@@ -54,14 +124,60 @@ class Enclosure():
     def get_name(self):
         return self.__name
 
-
-
-
     def set_cleanliness(self, cleanliness):
         if 1<= cleanliness <= 5:
             self.__cleanliness = cleanliness
         else:
             print('cleanliness must be between 1 and 5 stars')
+
+    def add_animal(self,animal):
+        '''
+        A method to add an animal to the enclosure if it complies with rules of environment, size, species mixing and
+        animals under treatment
+        :param animal: object of class Animal
+        :return: KeyError or message explaining if successful or unsuccessful in adding animal to enclosure
+        '''
+        if animal.health_status == 'under treatment':
+            # animals under treatment cannot be moved, check health status
+            return(f'{animal.name} is currently under treatment and cannot be moved')
+        else:
+            if self.animals == [] or self.animals[0].species == animal.species:
+                # Enclosures cannot hold more than 1 species, check if enclosure is empty or if existing animal
+                # is the same species as the animal being added
+                if len(self.animals) == self.size_limits[self.size]:
+                    # size of enclosure dictates number of animals which can be held. Check enclosure capacity
+                    return(f'This enclosure is {self.size} and has a maximum capacity of {self.size_limits[self.size]}'
+                          f'\n{animal} cannot be added')
+                else:
+                    try:
+                        # check the environment_animals dictionary to see what environment the species being added needs
+                        # within try/catch in case of key error. species may not have been added to dictionary yet
+                        if self.environment_animals[animal.species] != self.__environment:
+                            return(f'{animal.species}s cannot be kept in this environment. Cannot be added')
+                        else:
+                            self.__animals.append(animal)
+                            return(f'{animal.name} has been added to this enclosure')
+                    except KeyError:
+                        # if cannot find the animal in the environment_animals dictionary, raise KeyError and display messsage
+                        raise KeyError('Cannot find environment for this animal')
+            else:
+                # if enclosure is currently holding animal of another species, return warning message
+                return(f'Species is incompatible with current residents')
+
+
+    def remove_animal(self,animal):
+        '''
+        A method to remove an animal from the enclosure if it exists within the enclosure
+        :param animal: object of class Animal
+        :return: Message advising of successful or unsuccessful removal of animal from enclosure
+        '''
+        if animal in self.animals:
+            # check if animal is in this enclosure before removing
+            self.animals.remove(animal)
+            return(f'{animal.name} has been removed from this enclosure')
+        else:
+            # if animal is not here then return warning message
+            return(f'{animal} not found in this enclosure')
 
 
     name = property(get_name)
@@ -73,9 +189,4 @@ class Enclosure():
 
 
 
-e1 = Enclosure('lions1','Large','Savannah')
-print(e1)
-e2 = Enclosure('birds1','Small','Aviary')
-print(e2)
-e3 = Enclosure('lizards1','Mm','hothouse')
 
